d23d4ad85515e6b5c09a3086da3ce9b1f384f9bc
codongmin <codongmin@gmail.com>
2022-03-17 14:32:33 +0900

    Feat: Modified users app admin panel
    1. 유저 앱의 admin panel을 커스텀, admin에 등록
    
    -

--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from . import models
 
 # Register your models here.
 @admin.register(models.User)
-class CustomUserAdmin(admin.ModelAdmin):
-    pass
+class CustomUserAdmin(UserAdmin):
+    
+    """ Custom User Admin """
+    
+    custom_fieldsets = (
+        ('Custom Profile', {'fields': ('avatar', 'gender', 'bio', 'birthdate', 'language', 'currency', 'superhost')}),
+    )
+
+    fieldsets = custom_fieldsets + UserAdmin.fieldsets
+    
+
+    custom_display = ('username', 'language', 'currency', 'superhost')
+    custom_filter = ('superhost', 'gender', 'language', 'currency', 'superhost')
+
+    list_display = custom_display + UserAdmin.list_display
+    list_filter = custom_filter + UserAdmin.list_filter
+    
+    
