389e59fd60c8ab2c2baf3e13f8427b293f87d6cd
codongmin <codongmin@gmail.com>
2022-05-13 07:51:39 +0900

    custom room admin

--- rooms/admin.py
+++ rooms/admin.py
@@ -14,8 +14,58 @@ class ItemAdmin(admin.ModelAdmin):
 class RoomAdmin(admin.ModelAdmin):
 
     """ Room Admin Definition """
+    # https://docs.djangoproject.com/en/4.0/ref/contrib/admin/#modeladmin-options
 
-    pass
+    fieldsets = (
+        (
+            "Basic Info",
+            {"fields": ('name', 'description')}
+        ),
+    )
+
+
+    # object outside
+    list_display = (
+        "name",
+        "country",
+        "city",
+        "price",
+        "guests",
+        "beds",
+        "bedrooms",
+        "bath",
+        "check_in",
+        "check_out",
+        "instant_book"
+    )
+    
+    list_filter = (
+        'instant_book',
+        'host__superhost',
+        'room_type',
+        'amenities',
+        'facilities',
+        'house_rules',
+        'city',
+        'country'
+    )
+
+
+    search_fields = ['=city', '^host__username'] # 외래키로 연결된 field의 항목들도 검색도 가능, foreignkey__field, 정확한 값을 찾으려면 __exact 접미사를 붙이면 됨.
+    """
+    Useful prefix 
+    ^ : startwith
+    = : iexact 
+    @ : search
+    none : icontain
+    """
+
+    # object inside
+    filter_horizontal = (
+        'amenities',
+        'facilities',
+        'house_rules',
+    )
 
 
 @admin.register(models.Photo)
