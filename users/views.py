86a0786333e029041c97258f04665c52588748cb
codongmin <codongmin@gmail.com>
2022-06-07 23:07:21 +0900

    Add: login/logout 기능

--- users/views.py
+++ users/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from cmath import log
+from django.urls import reverse
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.views import View
+from . import forms
 
 # Create your views here.
+class LoginView(View):
+    def get(self, request):
+        form = forms.LoginForm(initial={'email': 'codongmin@gmail.com'})
+        # NomadCoder1
+        return render(request, 'users/login.html', {'form':form})
+
+    def post(self, request):
+        form = forms.LoginForm(request.POST)
+
+        if form.is_valid():
+            email = form.cleaned_data.get('email')
+            password = form.cleaned_data.get('password')
+
+            user = authenticate(request, username=email, password=password)
+
+            if user is not None:
+                login(request, user)
+                return redirect(reverse('core:home'))
+
+        return render(request, 'users/login.html', {'form':form})
+    
+def logout_view(request):
+    logout(request)
+    return redirect(reverse('core:home'))
