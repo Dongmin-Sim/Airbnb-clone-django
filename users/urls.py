6af798f467dc6f62132be0002c9797c9318816a8
codongmin <codongmin@gmail.com>
2022-06-09 22:04:57 +0900

    Style: form -> Model form

--- users/urls.py
+++ users/urls.py
@@ -5,5 +5,6 @@ app_name = 'users'
 
 urlpatterns = [
     path('logout/', views.logout_view, name='logout'),
-    path('login/', views.LoginView.as_view(), name='login'),
+    path('login/', views.LoginView.as_view(), name='login'), 
+    path('signup/', views.SignUpView.as_view(), name='signup'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,4 @@
+from tkinter.tix import Form
 from django.views.generic import FormView
 from django.urls import reverse, reverse_lazy
 from django.shortcuts import render, redirect
@@ -9,10 +10,10 @@ from . import forms
 class LoginView(FormView):
     template_name = 'users/login.html'
     form_class = forms.LoginForm
+    success_url = reverse_lazy('core:home')
     initial = {
         'email': 'codongmin@gmail.com'
     }
-    success_url = reverse_lazy('core:home')
 
     def form_valid(self, form):
         email = form.cleaned_data.get('email')
@@ -29,3 +30,26 @@ class LoginView(FormView):
 def logout_view(request):
     logout(request)
     return redirect(reverse('core:home'))
+
+
+class SignUpView(FormView):
+    template_name = 'users/signup.html'
+    form_class = forms.SignUpForm
+    success_url = reverse_lazy('core:home')
+    initial = {
+        'first_name':'sim',
+        'last_name': 'dongmin',
+        'email': 'codongmin@gmail.com'
+    }
+
+    def form_valid(self, form):
+        form.save()
+        
+        email = form.cleaned_data.get('email')
+        password = form.cleaned_data.get('password')
+
+        user = authenticate(self.request, username=email, password=password)
+
+        if user is not None:
+            login(self.request, user)
+        return super().form_valid(form)
